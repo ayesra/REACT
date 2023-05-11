4197237a24d437c16c140f25d806880ba662779e
Esra_Ay <esragedikliay@gmail.com>
2023-05-11 12:40:02 +0200

    react-sass

---
+++ class-notes/DE-03-react-sass/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import './scss/_button.scss'
+// import './scss/_reset.scss'
+// import './scss/_variable.scss'
+
+import "./App.scss"
+import Header from "./components/header/Header";
+import Card from "./components/card/Card";
+import dataa from "./data"
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Card datA={dataa}/>
+    </div>
+  );
+}
+
+export default App;
