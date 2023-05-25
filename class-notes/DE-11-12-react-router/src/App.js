bcb2ecb4f18a6f6f84f909ee9f3854af6e639d24
Esra_Ay <esragedikliay@gmail.com>
2023-05-25 12:23:38 +0200

    react-router

--- class-notes/DE-11-12-react-router/src/App.js
+++ class-notes/DE-11-12-react-router/src/App.js
@@ -3,16 +3,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import MyNavbar from './components/MyNavbar';
 import Footer from './components/Footer';
-
+import Home from './pages/Home'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Teacher from './pages/Teacher';
+import CourseCard from './pages/CourseCard'
+import ContactForm from './pages/ContactForm';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <div>
+      <Router>
         <MyNavbar />
 
-       
-
+        <Routes>
+          {/* "/"" (ana yol) tüm yollara dahil edilmiştir, bu nedenle onu
+         "/"" başlayan diğer yollardan ayırt etmek için exact anahtar kelimesine sahip olması gerekir . */}
+          <Route path="/" element={<Home />} />
+          <Route path="/teacher" element={<Teacher />} />
+          <Route path="/courses" element={<CourseCard />} />
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
         <Footer />
+      </Router>
     </div>
   );
 }
