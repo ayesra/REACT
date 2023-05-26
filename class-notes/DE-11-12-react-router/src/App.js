c0407e41df1d33bd2bf17b3ac344bf292fc22e19
Esra_Ay <esragedikliay@gmail.com>
2023-05-26 11:20:07 +0200

    teacherDetails

--- class-notes/DE-11-12-react-router/src/App.js
+++ class-notes/DE-11-12-react-router/src/App.js
@@ -9,6 +9,8 @@ import Teacher from './pages/Teacher';
 import CourseCard from './pages/CourseCard'
 import ContactForm from './pages/ContactForm';
 import NotFound from './pages/NotFound';
+import CardDetails from './pages/CardDetails';
+import TeacherDetails from './pages/TeacherDetails';
 
 const App = () => {
   return (
@@ -19,9 +21,12 @@ const App = () => {
         <Routes>
           {/* "/"" (ana yol) tüm yollara dahil edilmiştir, bu nedenle onu
          "/"" başlayan diğer yollardan ayırt etmek için exact anahtar kelimesine sahip olması gerekir . */}
+          {/* "/courses/:name"--->böyle bir yolda yani':' kisim bir degisken belirtir,yani name=HtTML veya =CSS gibi  */}
           <Route path="/" element={<Home />} />
           <Route path="/teacher" element={<Teacher />} />
+          <Route path="/teacher/:id" element={<TeacherDetails/>}/>
           <Route path="/courses" element={<CourseCard />} />
+          <Route path="/courses/:name" element={<CardDetails/>}/>
           <Route path="/contact" element={<ContactForm />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
--- class-notes/DE-11-12-react-router/src/pages/CardDetails.jsx
+++ class-notes/DE-11-12-react-router/src/pages/CardDetails.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-
 import data from "../data"
-const CardDetails = () => {
+import { useNavigate, useParams } from 'react-router-dom';
 
-  
+const CardDetails = () => {
+  const navigate = useNavigate()
+  const { name } = useParams()
+  console.log(name);
+
   return (
     <div className="container text-center mt-4">
-  CardDetails
+
+      {data.map((i) => i.name === name && (<div>
+        <h3>{i.name}</h3>
+        <img src={i.img} alt="" />
+        <h3>{i.text}</h3>
+        <h4>{i.yorum}</h4>
+        <div>
+          <button className="btn btn-primary" onClick={() => navigate(-1)}>
+            GO BACK
+          </button>
+          <button className="btn btn-warning" onClick={() => navigate("/")}>
+            GO HOME
+          </button>
+        </div>
+      </div>))}
+
     </div>
   );
 }
