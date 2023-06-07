d4a96d9812528142a57e42ee472d4564ec73e027
Esra_Ay <esragedikliay@gmail.com>
2023-06-07 11:31:53 +0200

    TailwindCss-files

---
+++ class-notes/DE-20-TailwindCss-Kurulum/src/App.js
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div className="main">
+      {/* <h1 className="text-3xl font-bold underline">Hello</h1> */}
+      {/* <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 p-4">
+      <h2>Title</h2>
+        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg" alt="" className="h-[250px] rounded-xl" />
+      <p className="text-white line-clamp-1 hover:line-clamp-none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, voluptatibus! Sequi necessitatibus et dolorum vel doloribus, ad, magni quasi explicabo, eaque quia cupiditate. Corporis, rem?</p>
+      </div> */}
+      
+      {/* <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 p-4">
+      <h2>Title</h2>
+        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg" alt="" className="h-[250px] rounded-xl" />
+      <p className="text-white line-clamp-1 hover:line-clamp-none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, voluptatibus! Sequi necessitatibus et dolorum vel doloribus, ad, magni quasi explicabo, eaque quia cupiditate. Corporis, rem?</p>
+      </div> */}
+      <div className="card">
+      <h2>Title</h2>
+        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg" alt="" />
+      <p className="text-white line-clamp-1 hover:line-clamp-none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, voluptatibus! Sequi necessitatibus et dolorum vel doloribus, ad, magni quasi explicabo, eaque quia cupiditate. Corporis, rem?</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
